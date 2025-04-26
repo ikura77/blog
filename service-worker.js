d38bf05821cf131463678260eb865a11da0c676c
ikura77 <pedehi@na-cat.com>
2025-04-27 00:11:48 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'diary-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './editor.html',
+  './drafts.html',
+  './article.html',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// インストール時
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// ネットワークが使えないときキャッシュを返す
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
